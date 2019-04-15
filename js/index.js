26bb967a5f880e8197fd22de39955e8e8afd457a
Paul Abernathy <paul.abernathy7@gmail.com>
2019-04-14 23:17:27 -0400

    added events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+const nav = document.querySelector(".nav")
+
+nav.addEventListener('mouseover', function(event){
+    event.target.style.color = "blue";
+})
+
+const header = document.querySelector('html')
+
+header.addEventListener('keydown', function(event) {
+    event.target.style.color = "aqua";
+ 
+})
+header.addEventListener('scroll', function(event) {
+    event.target.style.fontFamily = 'Helvetica'
+})
+
+const button = document.querySelector('.btn')
+
+button.addEventListener('dblclick', function(event) {
+    event.target.style.color = 'red';
+})
+
+const image = document.querySelector('.bus')
+
+image.addEventListener('click', function(event) {
+    event.target.style.display = 'none'
+
+})
