8c6e46ee8f67b7f4b4d118fe739ac407a6952e60
Paul Abernathy <paul.abernathy7@gmail.com>
2019-04-15 00:36:19 -0400

    2nd commit

--- js/index.js
+++ js/index.js
@@ -6,19 +6,25 @@ nav.addEventListener('mouseover', function(event){
     event.target.style.color = "blue";
 })
 
-const header = document.querySelector('html')
+const background = document.querySelector('html')
 
-header.addEventListener('keydown', function(event) {
+background.addEventListener('keydown', function(event) {
     event.target.style.color = "aqua";
  
 })
-header.addEventListener('scroll', function(event) {
+background.addEventListener('scroll', function(event) {
     event.target.style.fontFamily = 'Helvetica'
 })
 
 const button = document.querySelector('.btn')
 
+button.parentElement.addEventListener('dblclick', function(event){
+    event.target.style.color = "yellow";
+
+})
+
 button.addEventListener('dblclick', function(event) {
+    event.stopPropagation()
     event.target.style.color = 'red';
 })
 
@@ -28,3 +34,15 @@ image.addEventListener('click', function(event) {
     event.target.style.display = 'none'
 
 })
+
+const header = document.querySelectorAll('h2')
+header.forEach(function(element){
+    element.addEventListener('mousemove', function(event) {
+        event.target.style.color = 'green';
+    }) 
+    element.addEventListener('mouseout', function(event) {
+        event.target.style.color = "black"
+    })
+})
+
+
